9d9ed4cf53d7805d995e96e6cd2552a680bc08c7
DESKTOP-OQV2L16\Alex <alexstk@hotmail.com>
2019-08-01 06:33:10 -0500

    12. Directiva estructural *ngFor: correccion

--- angular/clientes-app/src/app/directiva/directiva.component.ts
+++ angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directiva',
   templateUrl: './directiva.component.html',
-  styleUrls: ['./directiva.component.css']
 })
 export class DirectivaComponent {
 
