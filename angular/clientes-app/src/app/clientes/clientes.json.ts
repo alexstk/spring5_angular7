3ac822137d5c68b686d0e9f2c22f5a767ec584f5
DESKTOP-OQV2L16\Alex <alexstk@hotmail.com>
2019-08-24 19:30:02 -0500

    16. Creando archivo clientes.json.ts con la lista de objetos

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,17 @@
+import { Cliente } from './cliente';
+
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombre: 'Andrés', apellido: 'Guzmán', email: 'profesor@bolsadeideas.com', createAt: '2017-12-11' },
+    { id: 2, nombre: 'Mr. John', apellido: 'Doe', email: 'john.doe@gmail.com', createAt: '2018-01-02' },
+    { id: 3, nombre: 'Linux', apellido: 'Torvalds', email: 'linux.torvals@gmail.com', createAt: '2018-01-03' },
+    { id: 4, nombre: 'Rasmus', apellido: 'Lerdorff', email: 'rasmus.lerdorff@gmail.com', createAt: '2018-01-04' },
+    { id: 5, nombre: 'Erich', apellido: 'Gamma', email: 'erich.gamma@gmail.com', createAt: '2018-02-01' },
+    { id: 6, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2018-02-10' },
+    { id: 7, nombre: 'Ralf', apellido: 'Johnson', email: 'ralf.johson@gmail.com', createAt: '2018-02-18' },
+    { id: 8, nombre: 'John', apellido: 'Vlissides', email: 'john.vlissides@gmail.com', createAt: '2018-02-28' },
+    { id: 9, nombre: 'James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2018-03-03' },
+    { id: 10, nombre: 'Magma', apellido: 'Lee', email: 'magma.lee@gmail.com', createAt: '2018-03-04' },
+    { id: 11, nombre: 'Tornado', apellido: 'Roe', email: 'tornado.roe@gmail.com', createAt: '2018-03-05' },
+    { id: 12, nombre: 'Jade', apellido: 'Doe', email: 'jade.doe@gmail.com', createAt: '2018-03-06' },
+];
