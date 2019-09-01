0736c3da37f5a6ce1771cb66fe3ae1010e15403c
DESKTOP-OQV2L16\Alex <alexstk@hotmail.com>
2019-09-01 17:00:31 -0500

    39. Implementando Servicio Angular con HttpClient: Haciendo cast con el map operator de rxjs/operators

--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8090/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
-  }  
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[]));
+  }
 }
