a79f0421308655ed72cd4e0104faa643f6fdc523
alex_ossa <alexander.ossa.urrego@gmail.com>
2019-11-29 07:13:27 -0500

    72. Registrando el Locale y los diferentes Pattern para formatear fecha.

--- angular/clientes-app/src/app/app.module.ts
+++ angular/clientes-app/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { formatDate, DatePipe } from '@angular/common';
+
 import { Cliente } from './cliente';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -22,8 +23,8 @@ export class ClienteService {
         let clientes = response as Cliente[];
         return clientes.map(cliente => {
           cliente.nombre = cliente.nombre.toUpperCase();
-          let datePipe = new DatePipe('en-US');
-          cliente.createAt = datePipe.transform(cliente.createAt, 'dd/MM/yyyy');  //formatDate(cliente.createAt, 'dd-MM-yyyy', 'en-US')
+          let datePipe = new DatePipe('es');
+          cliente.createAt = datePipe.transform(cliente.createAt, 'EEEE dd, MMMM yyyy');  //formatDate(cliente.createAt, 'dd-MM-yyyy', 'en-US')
           return cliente;
         })
       })
