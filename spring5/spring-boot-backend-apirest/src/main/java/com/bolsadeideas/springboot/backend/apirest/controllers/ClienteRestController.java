b5ddfcd69e5fa1785674dbd2f25dd23b7747f09a
alex_ossa <alexander.ossa.urrego@gmail.com>
2019-12-05 06:53:32 -0500

    76. Implementando paginación en API REST y Repository

--- spring5/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ spring5/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -10,6 +10,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -22,7 +25,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
@@ -39,6 +41,12 @@ public class ClienteRestController {
 	@GetMapping("/clientes")
 	public List<Cliente> index(){
 		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/page/{page}")
+	public Page<Cliente> index(@PathVariable Integer page){
+		Pageable pageable = PageRequest.of(page, 4);
+		return clienteService.findAll(pageable);
 	}
 	
 	@GetMapping("/clientes/{id}")
--- spring5/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ spring5/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -2,11 +2,16 @@ package com.bolsadeideas.springboot.backend.apirest.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService {
 	
 	List<Cliente> findAll();
+	
+	Page<Cliente> findAll(Pageable pageable);
 	
 	Cliente findById(Long id);
 	
