c3d8a8cf9bd9d022509abd0d55e78ff68021bf46
alex_ossa <alexander.ossa.urrego@gmail.com>
2020-03-25 06:54:06 -0500

    101. Implemementando Modal como componente anidado (hijo)

--- angular/clientes-app/src/app/clientes/detalle/detalle.component.ts
+++ angular/clientes-app/src/app/clientes/detalle/detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute } from '@angular/router';
@@ -11,7 +11,8 @@ import { HttpEventType } from '@angular/common/http';
   styleUrls: ['./detalle.component.css']
 })
 export class DetalleComponent implements OnInit {
-  cliente: Cliente;
+  
+  @Input() cliente: Cliente;
   titulo: string = 'Detalle del Cliente';
   private fotoSeleccionada: File;
   private progreso: number = 0;
@@ -19,14 +20,7 @@ export class DetalleComponent implements OnInit {
   constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(params => {
-      let id: number = +params.get('id');
-      if (id) {
-        this.clienteService.getCliente(id).subscribe(cliente => {
-          this.cliente = cliente;
-        });
-      }
-    });
+    
   }
 
   seleccionarFoto(event) {
