8c561bb69e92f35adc64cae60f920869c560740e
alex_ossa <alexander.ossa.urrego@gmail.com>
2020-06-20 17:58:47 -0500

    152. Validando fecha de expiración del token en el AuthGuard

--- angular/clientes-app/src/app/usuarios/guards/auth.guard.ts
+++ angular/clientes-app/src/app/usuarios/guards/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import { SELECT_PANEL_VIEWPORT_PADDING } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +17,24 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authService.isAuthenticated()) {
+      if(this.isTokenExpirado()){
+        this.authService.logout();
+        this.router.navigate(['/login']);
+        return false;
+      }
       return true;
     }
     this.router.navigate(['/login']);
     return false;
   }
 
+  isTokenExpirado(): boolean {
+    let token = this.authService.token;
+    let payload = this.authService.obtenerDatosToken(token);
+    let now = new Date().getTime() / 1000;    // seconds 
+    if (payload.exp < now) {
+      return true;    // token has expired
+    }
+    return false;     // token hasn't expired
+  }
 }
