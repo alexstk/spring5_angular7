0d510be27af96cffd9b18008ba85a6998a643c5a
alex_ossa <alexander.ossa.urrego@gmail.com>
2020-07-20 16:38:35 -0500

    168. Añadiendo el detalle de la Factura

--- angular/clientes-app/src/app/facturas/detalle-factura.component.ts
+++ angular/clientes-app/src/app/facturas/detalle-factura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Factura } from './models/factura';
+import { ActivatedRoute } from '@angular/router';
+import { FacturaService } from './services/factura.service';
 
 @Component({
   selector: 'app-detalle-factura',
@@ -6,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleFacturaComponent implements OnInit {
 
-  constructor() { }
+  factura: Factura;
+  nombre: string = 'Factura';
+
+  constructor(private activatedRoute: ActivatedRoute, private facturaService: FacturaService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = +params.get('id');
+      this.facturaService.getFactura(id).subscribe(factura => this.factura = factura);
+    });
   }
 
 }
