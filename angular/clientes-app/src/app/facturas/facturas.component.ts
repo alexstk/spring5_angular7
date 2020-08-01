4bf81c9f74949bcf267fa11dcbc2abfe7a41eb2d
alex_ossa <alexander.ossa.urrego@gmail.com>
2020-08-01 14:34:54 -0500

    179. Incrementando la cantidad si el producto existe en el detalle

--- angular/clientes-app/src/app/facturas/facturas.component.ts
+++ angular/clientes-app/src/app/facturas/facturas.component.ts
@@ -55,9 +55,13 @@ export class FacturasComponent implements OnInit {
     let producto = event.option.value as Producto;
     console.log(producto);
 
-    let nuevoItem = new ItemFactura();
-    nuevoItem.producto = producto;
-    this.factura.items.push(nuevoItem);
+    if (this.exiteItem(producto.id)) {
+      this.incrementaCantidad(producto.id);
+    } else {
+      let nuevoItem = new ItemFactura();
+      nuevoItem.producto = producto;
+      this.factura.items.push(nuevoItem);
+    }
 
     this.autocompleteControl.setValue('');
     event.option.focus();
@@ -71,6 +75,25 @@ export class FacturasComponent implements OnInit {
     this.factura.items = this.factura.items.map((item: ItemFactura) => {
       if (id === item.producto.id) {
         item.cantidad = cantidad;
+      }
+      return item;
+    });
+  }
+
+  exiteItem(id: number): boolean {
+    let existe = false;
+    this.factura.items.forEach((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        existe = true;
+      }
+    });
+    return existe;
+  }
+
+  incrementaCantidad(id: number): void {
+    this.factura.items = this.factura.items.map(item => {
+      if (id === item.producto.id) {
+        ++item.cantidad;
       }
       return item;
     });
