04fc690359f74cd2a8b357f17696f7a5eadb80c5
alex_ossa <alexander.ossa.urrego@gmail.com>
2021-01-10 10:51:05 -0500

    213. Notificar cuando un usuario está escribiendo

--- angular/chat-app/src/app/chat/chat.component.ts
+++ angular/chat-app/src/app/chat/chat.component.ts
@@ -14,6 +14,7 @@ export class ChatComponent implements OnInit {
 
   mensaje: Mensaje = new Mensaje();
   mensajes: Mensaje[] = [];
+  escribiendo: string;
 
   constructor() { }
 
@@ -40,6 +41,11 @@ export class ChatComponent implements OnInit {
         console.log(mensaje);
       });
 
+      this.client.subscribe('/chat/escribiendo', e => {
+        this.escribiendo = e.body;
+        setTimeout(() => this.escribiendo = '', 3000);
+      });
+
       this.mensaje.tipo = "NUEVO_USUARIO";
       this.client.publish({ destination: '/app/mensaje', body: JSON.stringify(this.mensaje) });
     }
@@ -62,5 +68,9 @@ export class ChatComponent implements OnInit {
     this.mensaje.tipo = "MENSAJE";
     this.client.publish({ destination: '/app/mensaje', body: JSON.stringify(this.mensaje) });
     this.mensaje.texto = '';
+  }
+  
+  escribiendoEvento(): void{
+    this.client.publish({ destination: '/app/escribiendo', body: this.mensaje.username });
   }
 }
