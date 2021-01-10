b60ecc572d1957280fe80961e0c67e5fbe34b8c8
alex_ossa <alexander.ossa.urrego@gmail.com>
2021-01-10 10:40:21 -0500

    213. Notificar cuando un usuario está escribiendo

--- spring5/spring-boot-backend-chat/src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
+++ spring5/spring-boot-backend-chat/src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
@@ -25,5 +25,11 @@ public class ChatController {
 		
 		return mensaje;
 	} 
+	
+	@MessageMapping("/escribiendo")
+	@SendTo("/chat/escribiendo")
+	public String estaEscribiendo(String username) {
+		return username.concat(" está escribiendo...");
+	}
 
 }
