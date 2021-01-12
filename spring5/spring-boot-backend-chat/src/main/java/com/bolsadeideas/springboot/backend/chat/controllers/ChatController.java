bded8f145829e2a01140c85e664e789beef1f076
alex_ossa <alexander.ossa.urrego@gmail.com>
2021-01-12 07:36:43 -0500

    217. Agregando Clases del Modelo Document y Repository

--- spring5/spring-boot-backend-chat/src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
+++ spring5/spring-boot-backend-chat/src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
@@ -3,24 +3,32 @@ package com.bolsadeideas.springboot.backend.chat.controllers;
 import java.util.Date;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.bolsadeideas.springboot.backend.chat.models.documents.Mensaje;
+import com.bolsadeideas.springboot.backend.chat.models.service.ChatService;
 
 @Controller
 public class ChatController {
 	
 	private String[] colores = { "red", "green", "blue", "magenta", "purple", "orange" };
 	
+	@Autowired
+	private ChatService chatService;
+	
 	@MessageMapping("/mensaje")
 	@SendTo("/chat/mensaje")
 	public Mensaje recibirMensaje(Mensaje mensaje) {
 		mensaje.setFecha(new Date().getTime());
+		
 		if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
 			mensaje.setColor(colores[new Random().nextInt(colores.length)]);
 			mensaje.setTexto("nuevo usuario");
+		}else {
+			chatService.guardar(mensaje);
 		}
 		
 		return mensaje;
